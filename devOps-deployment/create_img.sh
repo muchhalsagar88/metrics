a6e35772969abf71ad40889c0b7a6eeb284d9630
Rohan Chakravarthy <rohanc219@gmail.com>
2015-11-18 19:26:51 -0500

    Deleted files

--- devOps-deployment/create_img.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker run -it -d --name $1 -p $2:5000 --link redis_ambassador:redis python-base sh scripts/run_server.sh $3
-
--- devOps-deployment/run_server.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-git clone -b $1 https://github.com/rchakra3/simple-flask-app.git
-cd simple-flask-app
-pip install -r requirements.txt
-python src/app.py
